b6bb0fb8ef2c94b0b1f2f13e6a05cb27f745b3eb
John Gleason <jogleasonjr@gmail.com>
2019-11-14 12:57:06 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -159,10 +159,10 @@ resource "azurerm_monitor_diagnostic_setting" "aks" {
 # Configure cluster
 
 provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.aks.kube_admin_config.host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.cluster_ca_certificate)
+  host                   = azurerm_kubernetes_cluster.aks.kube_admin_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.cluster_ca_certificate)
 }
 
 #
