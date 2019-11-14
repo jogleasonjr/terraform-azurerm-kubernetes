e66dc15f3f005b3f04a6ec2defde8f4e8742a313
John Gleason <jogleasonjr@gmail.com>
2019-11-14 10:52:36 -0600

    Update main.tf

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -2,26 +2,13 @@ module "simple" {
     source = "../../"
 
     name = "simple"
-    resource_group_name = "simple-aks-rg"
-    location = "westeurope"
-    service_cidr = "10.241.0.0/24"
+    resource_group_name = var.resource_group_name
+    location = var.location
+    service_cidr = var.vnet_address_range
     kubernetes_version = "1.13.5"
     
     service_principal = {
-        client_id = "00000000-0000-0000-0000-000000000000"
-        client_secret = "00000000-0000-0000-0000-000000000000"
+        client_id = var.client_id
+        client_secret = var.client_secret
     }
-
-    azure_active_directory = {
-        client_app_id = "00000000-0000-0000-0000-000000000000"
-        server_app_id = "00000000-0000-0000-0000-000000000000"
-        server_app_secret = "00000000-0000-0000-0000-000000000000"
-    }
-
-    agent_pools = [
-        {
-            name = "linux"
-            vnet_subnet_id = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/mygroup1/providers/Microsoft.Network/virtualNetworks/myvnet1"
-        },
-    ]
 }
